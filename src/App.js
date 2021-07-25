7cccc084e36863d651d86f8ed1fd51b3b37b8658
kane0505 <lahim.kane@outlook.com>
2021-07-24 18:30:48 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Main from './components/Main'
+
+class App extends Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+
+export default App;
